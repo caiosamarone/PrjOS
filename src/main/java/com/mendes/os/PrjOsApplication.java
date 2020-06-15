f08b4057ef25d523c123b8a788b70d29c2c4256d
Caio Mendes <caiosamarone10@hotmail.com>
2020-06-15 13:38:22 -0300

    First commit

---
+++ src/main/java/com/mendes/os/PrjOsApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendes/os/PrjOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
